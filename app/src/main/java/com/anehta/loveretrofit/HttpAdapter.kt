ac37832c30044a04df88ed79339ec459c70674bf
kof99athena <kof99athena@gmail.com>
2024-01-14 16:16:04 +0900

    retrofit

--- app/src/main/java/com/anehta/loveretrofit/HttpAdapter.kt
+++ app/src/main/java/com/anehta/loveretrofit/HttpAdapter.kt
@@ -8,18 +8,19 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.anehta.loveretrofit.databinding.RecyclerItemBinding
 
-class HttpAdapter constructor(val context : Context, val dataSet : ArrayList<HttpItem>) : Adapter<HttpAdapter.VH>() {
-//상속 받을때 반드시 생성자가 필요하다
-   inner class VH(binding: RecyclerItemBinding) : ViewHolder(binding.root){
-       //inner class는 리사이클러아이템을 말한다.
-       //binding으로 파라미터를 받고, 뷰홀더를 상속받는다.
-       val title : TextView by lazy { binding.tv }
-   }
+class HttpAdapter constructor(val context: Context, val dataSet: List<BikeStation>) :
+    Adapter<HttpAdapter.VH>() {
+    //상속 받을때 반드시 생성자가 필요하다
+    inner class VH(binding: RecyclerItemBinding) : ViewHolder(binding.root) {
+        //inner class는 리사이클러아이템을 말한다.
+        //binding으로 파라미터를 받고, 뷰홀더를 상속받는다.
+        val title: TextView by lazy { binding.tv }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         //레이아웃매니저를 호출한다. 신규 뷰를 만든다.
         //item 1개를 정의한다.
-        val binding = RecyclerItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        val binding = RecyclerItemBinding.inflate(LayoutInflater.from(context), parent, false)
         return VH(binding) //무엇을 리던하냐? 바인딩을 갖고있는 뷰홀더를 리턴한다.
     }
 
@@ -31,14 +32,7 @@ class HttpAdapter constructor(val context : Context, val dataSet : ArrayList<Htt
     override fun onBindViewHolder(holder: VH, position: Int) {
         //dataSet의 position에 맞게 요소들을 가져온다.
         //요소들을 재배치한다.
-        holder.title.text = dataSet[position].stationName
+        val bikeStation = dataSet[position]
+        holder.title.text = bikeStation.stationName
     }
-
-
-
-
-
-
-
-
 }
--- app/src/main/java/com/anehta/loveretrofit/HttpItem.kt
+++ app/src/main/java/com/anehta/loveretrofit/HttpItem.kt
@@ -2,12 +2,28 @@ package com.anehta.loveretrofit
 
 import com.google.gson.annotations.SerializedName
 
-data class HttpItem(
-    @SerializedName("rackTotCnt") val rackTotCnt : String,
-    @SerializedName("stationName") val stationName : String,
-    @SerializedName("parkingBikeTotCnt") val parkingBikeTotCnt : String,
-    @SerializedName("shared") val shared : String,
-    @SerializedName("stationLatitude") val stationLatitude : String,
-    @SerializedName("stationLongitude") val stationLongitude : String,
-    @SerializedName("stationId") val stationId : String,
+
+data class BikeStationResponse(
+    val rentBikeStatus: RentBikeStatus
+)
+
+data class RentBikeStatus(
+    val list_total_count: Int,
+    val RESULT: Result,
+    val row: List<BikeStation>
+)
+
+data class Result(
+    val CODE: String,
+    val MESSAGE: String
+)
+
+data class BikeStation(
+    val rackTotCnt: String,
+    val stationName: String,
+    val parkingBikeTotCnt: String,
+    val shared: String,
+    val stationLatitude: String,
+    val stationLongitude: String,
+    val stationId: String
 )
